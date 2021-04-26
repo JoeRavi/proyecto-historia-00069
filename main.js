3fab9de4b2a8d45d4d161eba6a9c9ad6f3fc4def
JoeRavi <estebanravazoli@gmail.com>
2021-04-26 04:26:16 -0600

    cambio2

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ var arr=contenido;
         $('.imagen').css('height','600px');
         $('.texto .text').append(arr[1]['texto']);
         $('.foto').append(arr[1]['imagenes']);
-        $('.foto img').css('max-height','40%')
+        $('.foto img').css('max-height','30%')
         $('.referencias .ref').append(arr[1]['referencias']);
         
         
@@ -53,7 +53,7 @@ var arr=contenido;
         $('.imagen').css('height','600px');
         $('.texto .text').append(arr[2]['texto']);
         $('.foto').append(arr[2]['imagenes']);
-        $('.foto img').css('max-height','40%')
+        $('.foto img').css('max-height','30%')
         $('.referencias .ref').append(arr[2]['referencias']);
         
         
@@ -67,7 +67,7 @@ var arr=contenido;
         $('.imagen').css('height','600px');
         $('.texto .text').append(arr[3]['texto']);
         $('.foto').append(arr[3]['imagenes']);
-        $('.foto img').css('max-height','40%')
+        $('.foto img').css('max-height','30%')
         $('.referencias .ref').append(arr[3]['referencias']);
         
         
@@ -81,7 +81,7 @@ var arr=contenido;
         $('.imagen').css('height','600px');
         $('.texto .text').append(arr[4]['texto']);
         $('.foto').append(arr[4]['imagenes']);
-        $('.foto img').css('max-height','40%')
+        $('.foto img').css('max-height','30%')
         $('.referencias .ref').append(arr[4]['referencias']);
         
         
@@ -95,7 +95,7 @@ var arr=contenido;
         $('.imagen').css('height','600px');
         $('.texto .text').append(arr[5]['texto']);
         $('.foto').append(arr[5]['imagenes']);
-        $('.foto img').css('max-height','40%')
+        $('.foto img').css('max-height','30%')
         $('.referencias .ref').append(arr[5]['referencias']);
         
         
@@ -109,7 +109,7 @@ var arr=contenido;
         $('.imagen').css('height','600px');
         $('.texto .text').append(arr[6]['texto']);
         $('.foto').append(arr[6]['imagenes']);
-        $('.foto img').css('max-height','40%')
+        $('.foto img').css('max-height','30%')
         $('.referencias .ref').append(arr[6]['referencias']);
         
         
@@ -123,7 +123,7 @@ var arr=contenido;
         $('.imagen').css('height','600px');
         $('.texto .text').append(arr[7]['texto']);
         $('.foto').append(arr[7]['imagenes']);
-        $('.foto img').css('max-height','40%')
+        $('.foto img').css('max-height','25%')
         $('.referencias .ref').append(arr[7]['referencias']);
         
         
@@ -137,7 +137,7 @@ var arr=contenido;
         $('.imagen').css('height','600px');
         $('.texto .text').append(arr[8]['texto']);
         $('.foto').append(arr[8]['imagenes']);
-        $('.foto img').css('max-height','40%')
+        $('.foto img').css('max-height','30%')
         $('.referencias .ref').append(arr[8]['referencias']);
         
         
@@ -151,7 +151,7 @@ var arr=contenido;
         $('.imagen').css('height','600px');
         $('.texto .text').append(arr[9]['texto']);
         $('.foto').append(arr[9]['imagenes']);
-        $('.foto img').css('max-height','40%')
+        $('.foto img').css('max-height','30%')
         $('.referencias .ref').append(arr[9]['referencias']);
         
         
@@ -165,7 +165,7 @@ var arr=contenido;
         $('.imagen').css('height','600px');
         $('.texto .text').append(arr[10]['texto']);
         $('.foto').append(arr[10]['imagenes']);
-        $('.foto img').css('max-height','40%')
+        $('.foto img').css('max-height','30%')
         $('.referencias .ref').append(arr[10]['referencias']);
         
         
